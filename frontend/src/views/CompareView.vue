dbe92a0f141fed5032d8dc26cd5b6074729043f1
Lukas Karras <karras.lukas@web.de>
2025-01-02 17:29:28 +0100

    added compare view structure

---
+++ frontend/src/views/CompareView.vue
@@ -0,0 +1,20 @@
+<template>
+	<CoreLayout>
+    <template #default>
+      <h1>Compare</h1>
+
+      <h1>TODO</h1>
+
+	  </template>
+	</CoreLayout>
+</template>
+
+<script>
+import CoreLayout from '@/components/CoreLayout.vue';
+
+export default{
+  components: {
+    CoreLayout
+  }
+}
+</script>
